1de7c0e5b9b2154276dde2d27ef00839c0a267d2
Alejo Canion <alejoidecano@gmail.com>
2025-06-11 18:11:21 +0800

    try number 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,16 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Permitir comillas no escapadas en JSX
-      "react/no-unescaped-entities": "off",
-      // Permitir imports no utilizados
-      "@typescript-eslint/no-unused-vars": "off",
-      // Permitir uso de `any`
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
 ];
 
 export default eslintConfig;
